0dea4f71181731c6ec28db09ad9d9892a4a74278
Pranav P <pranavkp.me@outlook.com>
2020-06-13 14:06:17 +0530

    resume download feature added

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CodingGif from './coding.gif';
+import resume from '../../static/pranav_p_cv.pdf';
 const Banner = () => (
   <section className="site-banner">
     <div className="container">
@@ -12,18 +13,24 @@ const Banner = () => (
             I'm a software engineer from India, specializing in building Full
             Stack and Progressive web app.
           </p>
-          <div className="site-buttons">
-            <div className="d-flex flex-row flex-wrap">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                className="btn btn-outline-secondary"
-                href="mailto:pranavkp.me@outlook.com"
-              >
-                Get in Touch
-              </a>
-            </div>
-          </div>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-outline-secondary"
+            href="mailto:pranavkp.me@outlook.com"
+          >
+            Get in Touch
+          </a>
+          &nbsp;&nbsp;
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-outline-info"
+            href={resume}
+            download
+          >
+            Resume
+          </a>
         </div>
         <div className="col-lg-6 col-md-12 banner-image">
           <CodingGif />
